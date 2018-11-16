3e7871f1750ed0a1c588cb8bdd76164548de0376
Andrey <dreykiman@gmail.com>
2018-11-16 08:24:28 +0000

    destructuring assignments in listener

--- src/connection/listener.js
+++ src/connection/listener.js
@@ -1,5 +1,5 @@
 import orderbook, {subscriptions} from '../market/orderbook'
-
+import {testNested} from '../utils/helpers'
 
 export default ev => {
   let data
@@ -10,52 +10,55 @@ export default ev => {
     throw {err: 'return value is not valid JSON'}
   }
 
-  if (data.event && data.channel) {
+  if (data.event && data.channel && data.event.payload) {
 //console.log(ev.data)
-    if (data.event.type === 'ORDER_CANCELLED') {
-      let pld = data.event.payload
+    let {payload: pld, type} = data.event
+
+    if (type === 'ORDER_CANCELLED') {
 //      if (pld) console.log(`${pld.status} ${pld.pairName} ${pld.pricepoint} ${pld.side} ${pld.baseToken}`)
 
       let prm = orderbook[pld.hash].cancelled
       if (prm && prm.resolve) prm.resolve(data)
 
-    } else if (data.event.type === 'ORDER_ADDED') {
-      let pld = data.event.payload
+    } else if (type === 'ORDER_ADDED') {
 //      if (pld) console.log(`${pld.status} ${pld.pairName} ${pld.pricepoint} ${pld.side} ${pld.baseToken}`)
 
       let prm = orderbook[pld.hash].added
       if (prm && prm.resolve) prm.resolve(data)
 
     } else if (data.channel === 'raw_orderbook') {
-      let pld = data.event.payload
-
-      if (pld && data.event.type === 'INIT') {
+      let ords = pld
+      if (type === 'INIT') {
         let pairName = pld.pairName
-        pld = pld.orders
+        ords = pld.orders
         if (subscriptions[pairName] && subscriptions[pairName].resolve) subscriptions[pairName].resolve()
       }
 
-      if (pld) {
-        pld.forEach( ord => {
-          ord = Object.assign(orderbook[ord.hash], ord) 
-          if (ord.added && ord.added.resolve) {
+      if (ords) {
+        ords.forEach( ord => {
+          let {added, cancelled} = Object.assign(orderbook[ord.hash], ord) 
+
+          if (added && added.resolve) {
 //            console.log(`from raw: ${ord.status} ${ord.pairName} ${ord.pricepoint} ${ord.side} ${ord.baseToken}`)
-            ord.added.resolve({event: {payload: ord}})
+            added.resolve({event: {payload: orderbook[ord.hash]}})
           }
-          if (ord.cancelled && ord.cancelled.resolve) {
+          if (cancelled && cancelled.resolve) {
 //            console.log(`from raw: ${ord.status} ${ord.pairName} ${ord.pricepoint} ${ord.side} ${ord.baseToken}`)
-            ord.cancelled.resolve({event: {payload: ord}})
+            cancelled.resolve({event: {payload: orderbook[ord.hash]}})
           }
         })
       }
-    } else if (data.event.type === 'ERROR') {
-      if (data.event.payload.includes && data.event.payload.includes("No order with this hash present")){
-        let hash = data.event.payload.split(":").slice(1)
-        let ord = orderbook[hash]
-        if (ord.cancelled && ord.cancelled.resolve) 
-          ord.cancelled.resolve({event: {payload: ord}})
+    } else if (type === 'ERROR') {
+      let { message='default', hash } = pld
+
+      if ( message.includes("No order with this hash present")
+      || message.includes("i/o timeout") ) {
+        let {cancelled, added} = orderbook[hash]
+        if (cancelled && cancelled.resolve) 
+          cancelled.resolve({event: {payload: orderbook[hash]}})
         if (ord.added && ord.cancelled.resolve) 
-          ord.added.resolve({event: {payload: ord}})
+          ord.added.resolve({event: {payload: orderbook[hash]}})
+        delete orderbook[hash]
       }
       console.log(data)
     } else {
