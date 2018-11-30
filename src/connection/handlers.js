387c1808ac9a7ba9587a3ebaa241039d65883816
Andrey <dreykiman@gmail.com>
2018-11-30 06:20:06 +0000

    check var existence before calling

--- src/connection/handlers.js
+++ src/connection/handlers.js
@@ -6,7 +6,7 @@ export const order_cancelled = data => {
 //  if (pld) console.log(`${pld.status} ${pld.pairName} ${pld.pricepoint} ${pld.side} ${pld.baseToken}`)
   let {cancelled} = orderbook[pld.hash]
 
-  if (cancelled) cancelled.resolve(data)
+  if (cancelled && cancelled.resolve) cancelled.resolve(data)
 }
 
 export const order_added = data => {
@@ -14,14 +14,14 @@ export const order_added = data => {
 //  if (pld) console.log(`${pld.status} ${pld.pairName} ${pld.pricepoint} ${pld.side} ${pld.baseToken}`)
   let {added} = orderbook[pld.hash]
 
-  if (added) added.resolve(data)
+  if (added && added.resolve) added.resolve(data)
 }
 
 export const order_pending = data => {
   let taker = data.event.payload.matches.takerOrder
   let {added} = Object.assign(orderbook[taker.hash], taker)
 
-  if (added) added.resolve({event: {payload: orderbook[taker.hash]}})
+  if (added && added.resolve) added.resolve({event: {payload: orderbook[taker.hash]}})
   orderbook[taker.hash].pending = new deferred(60000)
 }
 
@@ -29,16 +29,16 @@ export const order_success = data => {
   let taker = data.event.payload.matches.takerOrder
   let {added, pending} = Object.assign(orderbook[taker.hash], taker)
 
-  if (added) added.resolve({event: {payload: orderbook[taker.hash]}})
-  if (pending) pending.resolve({event: {payload: orderbook[taker.hash]}})
+  if (added && added.resolve) added.resolve({event: {payload: orderbook[taker.hash]}})
+  if (pending && pending.resolve) pending.resolve({event: {payload: orderbook[taker.hash]}})
 }
 
 export const resolve_from_book = ords => {
   ords.forEach( ord => {
     let {added, cancelled} = Object.assign(orderbook[ord.hash], ord) 
 
-    if (added) added.resolve({event: {payload: orderbook[ord.hash]}})
-    if (cancelled) cancelled.resolve({event: {payload: orderbook[ord.hash]}})
+    if (added && added.resolve) added.resolve({event: {payload: orderbook[ord.hash]}})
+    if (cancelled && cancelled.resolve) cancelled.resolve({event: {payload: orderbook[ord.hash]}})
   })
 }
 
@@ -46,8 +46,8 @@ export const reject_on_error = pld => {
   let { message, hash } = pld
   let {cancelled, added} = orderbook[hash]
 
-  if (cancelled) cancelled.reject(message)
-  if (added) added.reject(message)
+  if (cancelled && cancelled.reject) cancelled.reject(message)
+  if (added && added.reject) added.reject(message)
 
   delete orderbook[hash]
 }
