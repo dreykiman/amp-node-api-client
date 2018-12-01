96646e5eebe415eda8bb8a0345d092f7a0e40ab7
Andrey <dreykiman@gmail.com>
2018-12-01 06:17:25 +0000

    remove debug output

--- src/utils/deferred.js
+++ src/utils/deferred.js
@@ -7,7 +7,6 @@ export default class {
 
     if (timeOut != null) {
       setTimeout( _=> {
-        console.log( "Promise rejected due to expiration timer" )
         this.reject( {err: "rejected due to expiration timer"} )
       }, timeOut )
     }
