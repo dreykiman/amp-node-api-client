df7d6eba0ee466737700cac2574d3c855c8fb417
Andrey <dreykiman@gmail.com>
2018-12-27 06:49:34 +0000

    moved from binance to cryptocompare

--- src/binance/index.js
+++ src/binance/index.js
@@ -16,40 +16,34 @@ const client = Binance()
  * @returns {Array.<Promise>} returns array of promises, where each promise returns the lists of new bid and ask amounts/prices based on pulled binance orderbook
  */
 const createbooks = pairs => {
-  let scaleUSD = client.book({ symbol: 'TUSDETH' })
-    .then(reduce)
-    .then( ({bid,ask}) => (bid+ask)/2 )
-    .catch(msg => console.log({err: msg, log: 'TUSDETH'}))
+  let fsyms = pairs.map(ele=>{
+    let base = ele.pairName.split('/')[0]
+    return base==='WETH' ? 'ETH' : base
+  }).filter( (val,ind,self) => self.indexOf(val) === ind)
+  .join(',')
+
+  let tsyms = pairs.map(ele=>{
+    let quote = ele.pairName.split('/')[1]
+    return quote==='WETH' ? 'ETH' : quote
+  }).filter( (val,ind,self) => self.indexOf(val) === ind)
+  .join(',')
+
+  let book = rp("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+fsyms+"&tsyms="+tsyms, { json: true })
 
   return pairs.map( pair => {
     let pairName = pair.pairName
     let [base, quote] = pairName.split('/')
-    let symbol = base+'ETH'
 
-    let book = Promise.reject("Rejection that should never happen")
-    let scaleQuote = data => data
+    let minqty = 3
+    if(quote==="WETH") minqty = 0.03
 
-    if (pairName==='DAI/WETH') symbol = 'TUSDETH'
-    else if(pairName==='WETH/USDC') symbol = 'ETHUSDC'
-    else if(pairName==='MKR/WETH') symbol = 'mkreth'
-    else if(pairName==='MKR/USDC') symbol = 'mkrusd'
-    else if(pairName==='MKR/DAI') symbol = 'mkrusd'
-    else if (quote==='USDC' || quote==='DAI') {
-      symbol = base+'ETH'
-      scaleQuote = ({pair, bids, asks}) => scaleUSD.then( scale => {
-        bids = bids.map( ({price, qty}) => ({price:price/scale, qty}) )
-        asks = asks.map( ({price, qty}) => ({price:price/scale, qty}) )
-        return {pair, bids, asks}
-      })
-    }
+    base = base==='WETH' ? 'ETH' : base
+    quote = quote==='WETH' ? 'ETH' : quote
 
-    if (base==="MKR") book = rp('https://api.bitfinex.com/v1/book/'+symbol, { json: true })
-    else book = client.book({symbol})
-
-    return book.then(reduce)
+    return book.then(data => data[base][quote])
+      .then(price => ({bid:price, ask:price, minqty}))
       .then(makebook)
       .then(({bids, asks}) => ({pair, bids, asks}))
-      .then(scaleQuote)
   })
 }
 
--- src/binance/makebook.js
+++ src/binance/makebook.js
@@ -4,13 +4,13 @@
  * @alias module:binance.makebook
  * @param {number} bid - average bid price
  * @param {number} ask - average ask price
+ * @param {number} minqty - minimum order value
  * @property {number} minSpread=0.1 - minimum spread
- * @property {number} minQuantityInQuote=0.02|3 - minimum order value is 0.02 ETH or 3 USD
  * @property {number} nOrders=4 - number of bids/asks per pair
  * @property {number} depth=0.2 - factor to define depth of bid/ask
  * @returns {Array.<bids,asks>} returns list of bids and asks
  */
-const makebook = ({bid,ask}) => {
+const makebook = ({bid,ask,minqty}) => {
   let average = (bid+ask)/2
 
   let minSpread = 0.08
@@ -18,8 +18,6 @@ const makebook = ({bid,ask}) => {
   let bidStart = Math.min( (1-minSpread/2)*average, bid )
   let askStart = Math.max( (1+minSpread/2)*average, ask )
 
-  let minQuantityInQuote = 0.02
-  if(average>100) minQuantityInQuote = 3
   let nOrders = 4
   let depth = 0.1
 
@@ -28,7 +26,7 @@ const makebook = ({bid,ask}) => {
       let ieff = (iord + Math.random())/nOrders
 
       let qty = 1 + 3*ieff*ieff
-      qty *= minQuantityInQuote/average
+      qty *= minqty/average
 
       let step = ieff * depth
       step = ind===0 ? -step : step
