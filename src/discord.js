f3897d86c8f6e6b6b3fd46a59b0b70031dfc396f
Andrey <dreykiman@gmail.com>
2019-01-03 02:24:36 +0000

    add discord bot

---
+++ src/discord.js
@@ -0,0 +1,43 @@
+import { spawn } from 'child_process'
+import Discord from 'discord.io'
+import auth from '../auth.json'
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+})
+
+
+bot.on('ready', evt  => {
+  console.log("connected")
+})
+
+
+bot.on('message', (user, userID, to, message, evt) => {
+  if (message.substring(0, 1) == '!') {
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+    args = args.splice(1)
+
+    switch(cmd) {
+      case 'take':
+        console.log(cmd)
+        const taker = spawn( 'node', ['dist/taker'] )
+        taker.stdout.on( 'data', msg => bot.sendMessage({to, message: '`'+msg+'`'}) )
+        taker.on( 'close', _ => bot.sendMessage({to, message: ''}) )
+        break;
+      case 'make':
+        console.log(cmd)
+        const maker = spawn( 'node', ['dist/maker'] )
+        maker.stdout.on( 'data', msg => bot.sendMessage({to, message: '`'+msg+'`'}) )
+        maker.on( 'close', _ => bot.sendMessage({to, message: '================'}) )
+        break;
+    }
+
+//    if (cmd in commands) commands[cmd](user, userID, channelID, args)
+//    else commands.help(user, userID, channelID, args)
+  }
+})
+
+
