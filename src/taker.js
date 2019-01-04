538cf1898b2870813e2cac08a6cab1827a62ac9f
Andrey <dreykiman@gmail.com>
2019-01-04 08:14:28 +0000

    verbose error message

--- src/taker.js
+++ src/taker.js
@@ -16,7 +16,7 @@ Promise.all([amp.updateInfo(), amp.updatePairs(), amp.updateTokens()])
   .then( _ => amp.pairs.map(pair => amp.subscribe(pair)) )
   .then( arr => Promise.all(arr) )
   .then( _ => trader(wallet).takeone(process.argv[2]||"WETH/USDC") )
-  .catch(msg => console.log({err: msg.toString()}))
+  .catch(msg => console.log({err: JSON.stringify(msg), src: 'taker.js'}))
   .then(_=>ws.close())
  
 
