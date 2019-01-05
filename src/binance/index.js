e13a2ff15e236985bdd3782a091d349f5596163f
Andrey <dreykiman@gmail.com>
2019-01-05 09:31:00 +0000

    added preliminary flat prices method

--- src/binance/index.js
+++ src/binance/index.js
@@ -2,10 +2,8 @@
  * @module binance
  */
 import rp from 'request-promise-native'
-import Binance from 'binance-api-node'
 import {makebook} from './makebook'
-
-const client = Binance()
+import {prices as flats} from '../datastore/firedata'
 
 /**
  * creates the lists of bids and asks for each pair in the supplied list by pulling the orderbook for each pair from CryptoCompare, and then calling {@link module:binance.makebook|makebook} to create the list of bids and asks for AMP.
@@ -27,6 +25,17 @@ const createbooks = pairs => {
   .join(',')
 
   let book = rp("https://min-api.cryptocompare.com/data/pricemulti?fsyms="+fsyms+"&tsyms="+tsyms, { json: true })
+  const flatprices = rp("https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DAI&tsyms=USDC", {json:true})
+    .then(scl => flats.then(flt => {
+      return Object.keys(flt).reduce( (coll, pairName) => {
+        const [base,quote] = pairName.split('/')
+        coll[base] = { 'USDC': flt[pairName],
+          'DAI': flt[pairName]/scl.DAI.USDC,
+          'ETH': flt[pairName]/scl.ETH.USDC }
+        return coll
+      }, {})
+    }))
+  book = Promise.all([book, flatprices]).then( ([bk, flt]) => Object.assign(bk, flt) )
 
   return pairs.map( pair => {
     let pairName = pair.pairName
